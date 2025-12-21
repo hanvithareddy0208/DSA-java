972650f3f89335771c867ed25a6ae3abe69f6fd7
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2025-12-21 21:47:11 +0530

    update

--- basics.java
+++ basics.java
@@ -268,3 +268,22 @@
 //         }
 //     }
 // }
+
+//17.fibanacci series
+// import java.util.Scanner;
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int a =0;
+//         int b=1;
+//         int c;
+//         System.out.print(a+" "+b);
+//         for(int i=2;i<=n;i++){
+//             c = a+b;
+//             System.out.print(" "+c);
+//             a=b;
+//             b=c;
+//         }
+//     }
+// }
