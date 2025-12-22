e0c2664c1e51aba8406f7553b37dabd981a26a9a
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2025-12-22 21:57:13 +0530

    update

--- basics.java
+++ basics.java
@@ -302,4 +302,17 @@
 //     }
 // }
 
-//19.
+//19.power
+// import java.util.Scanner;
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int result =1;
+//         for(int i=1;i<=b;i++){
+//             result = result*a;
+//         }
+//         System.out.println(result);
+//     }
+// }
