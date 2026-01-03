36fdda3d4485d7df1892f142cf8e1ceae68827c6
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-01-03 22:31:31 +0530

    update

--- arrays.java
+++ arrays.java
@@ -14,3 +14,20 @@
 //     }
 // }
 
+//2.array sum
+// import java.util.Scanner;
+// public class arrays{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[]=new int[a];
+//         int sum=0;
+//         for(int i=0;i<a;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<a;i++){
+//             sum = sum+arr[i];
+//         }
+//         System.out.println(sum);
+//     }
+// }
