cf38fd679cf790caebcd3c06e9d3717732f9300b
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-01-03 22:19:27 +0530

    upadte

--- arrays.java
+++ arrays.java
@@ -70,3 +70,25 @@
 //         }
 //     }
 // }
+
+//5.positive or negative
+// import java.util.Scanner;
+// public class arrays{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[]=new int[a];
+//         for(int i=0;i<a;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<a;i++){
+//             if(arr[i]>0){
+//                 System.out.println("positive");
+//             }else if(arr[i]<0){
+//                 System.out.println("negative");
+//             }else{
+//                 System.out.println("zero");
+//             }
+//         }
+//     }
+// }
