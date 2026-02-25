f663928093747a866fb80980462d6a708db2a270
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-25 23:04:08 +0530

    update

---
+++ pattern.java
@@ -0,0 +1,32 @@
+//patterns
+//1.regular pattern
+
+//1.regular pattern
+// public class pattern{
+//     public static void main(String[]args){
+//         int n =4;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//2.hollow reactangle
+// public class pattern{
+//     public static void main(String[]args){
+//         int n = 4;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(i==1||j==1||j==n||i==n){
+//                     System.out.print("*");
+//                 }else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
