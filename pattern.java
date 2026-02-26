45c912c3eb139f0dbf4cbdff7e3f7f27a6fda5b3
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-26 22:11:36 +0530

    update

---
+++ pattern.java
@@ -0,0 +1,12 @@
+//1.regular pattern
+// public class pattern{
+//     public static void main(String[]args){
+//         int n =4;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
