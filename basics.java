845828ecdebe281e09fbcae4f04ba2395b80314f
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-27 21:19:42 +0530

    update

--- basics.java
+++ basics.java
@@ -173,3 +173,17 @@
 // }
 
 //12.sum of digits
+// import java.util.Scanner;
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int sum=0;
+//         while(a!=0){
+//             int rem = a%10;
+//             sum = sum+rem;
+//             a = a/10;
+//         }
+//         System.out.println(sum);
+//     }
+// }
