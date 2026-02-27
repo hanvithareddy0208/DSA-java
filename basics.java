918edcd5736cfa6bff85ab35d1bee99cf64b6bb7
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-27 21:25:00 +0530

    update

--- basics.java
+++ basics.java
@@ -187,3 +187,19 @@
 //         System.out.println(sum);
 //     }
 // }
+
+//13.reverse a number
+import java.util.Scanner;
+public class basics{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int rev=0;
+        while(a!=0){
+            int rem = a%10;
+            rev = rev*10+rem;
+            a = a/10;
+        }
+        System.out.println(rev);
+    }
+}
