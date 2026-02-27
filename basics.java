4aaf32a42a2b479557ab37094a483b1b3ab50bd9
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-27 21:51:04 +0530

    update

--- basics.java
+++ basics.java
@@ -287,3 +287,19 @@
 //         }
 //     }
 // }
+
+//18.factorial of a number
+// import java.util.Scanner;
+// public class basics{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int result =1;
+//         for(int i=1;i<=a;i++){
+//             result = result*i;
+//         }
+//         System.out.println(result);
+//     }
+// }
+
+//19.
