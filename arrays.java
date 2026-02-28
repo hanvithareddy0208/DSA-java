2bd99f7faaa2ad87409e9b22726f006237633eee
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-28 22:47:09 +0530

    update

--- arrays.java
+++ arrays.java
@@ -154,3 +154,23 @@
 //         }
 //     }
 // }
+
+//9.copied array
+// import java.util.Scanner;
+// public class arrays{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[]=new int[a];
+//         int copied[]=new int[a];
+//         for(int i=0;i<a;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<a;i++){
+//             copied[i]=arr[i];
+//         }
+//         for(int i=0;i<a;i++){
+//             System.out.println(copied[i]);
+//         }
+//     }
+// }
