22233b13264fa90e5c4219f643bad7c62ae278a0
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-02-28 22:44:39 +0530

    update

--- arrays.java
+++ arrays.java
@@ -138,3 +138,19 @@
 //         System.out.println(min);
 //     }
 // }
+
+//8.reverse 
+// import java.util.Scanner;
+// public class arrays{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[]=new int[a];
+//         for(int i=0;i<a;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=a-1;i>=0;i--){
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
