36c059ea728cf5bf3d667ef6b4be0217e25c1419
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-03-04 22:36:30 +0530

    update

--- pattern.java
+++ pattern.java
@@ -30,3 +30,23 @@
 //         }
 //     }
 // }
+
+//4.even or odd
+// import java.util.Scanner;
+// public class arrays{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[]=new int[a];
+//         for(int i=0;i<a;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<a;i++){
+//             if(arr[i]%2==0){
+//                 System.out.println("even");
+//             }else{
+//                 System.out.println("odd");
+//             }
+//         }
+//     }
+// }
