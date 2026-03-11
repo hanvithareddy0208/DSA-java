db1b323e3a6f06216c593223e7bd85df5365fa49
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-03-11 22:14:54 +0530

    --date=01-03-2026

--- arrays.java
+++ arrays.java
@@ -31,3 +31,22 @@
 //         System.out.println(sum);
 //     }
 // }
+
+//3.average
+// import java.util.Scanner;
+// public class arrays{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[]=new int[a];
+//         int sum=0;
+//         for(int i=0;i<a;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<a;i++){
+//             sum = sum+arr[i];
+//         }
+//         int average =sum/a;
+//         System.out.println(average);
+//     }
+// }
